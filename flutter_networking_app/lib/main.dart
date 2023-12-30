2a3957f64d8c2893b934176ec4880c11174e60e9
Ramji Yadav <ramjiyadav22@iitk.ac.in>
2023-12-31 00:56:23 +0530

    A

--- flutter_networking_app/lib/main.dart
+++ flutter_networking_app/lib/main.dart
@@ -43,25 +43,6 @@ Future<Album> createAlbum(String title) async {
   }
 }
 
-// ... (Previous code remains unchanged)
-
-// ... (Remaining code remains unchanged)
-
-// Future<Album> deleteAlbum(String id) async {
-//   final http.Response response = await http.delete(
-//     Uri.parse('https://jsonplaceholder.typicode.com/albums/$id'),
-//     headers: <String, String>{
-//       'Content-Type': 'application/json; charset=UTF-8',
-//     },
-//   );
-
-//   if (response.statusCode == 200) {
-//     return Album.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
-//   } else {
-//     throw Exception('Failed to delete album.');
-//   }
-// }
-
 class Album {
   final int userId;
   final int id;
